fb827ec4ee8dc3fcef64a9e4705c871955ff643a
Jordi Bunster <jordi@bunster.org>
2008-10-03 15:02:11 -0400

    Add rails/init.rb for GemPlugins

--- init.rb
+++ init.rb
@@ -1,9 +1,1 @@
-require 'bunster/find_by_sql_file'
-
-class << ActiveRecord::Base
-  include Bunster::FindBySqlFile
-
-  alias_method_chain :find_by_sql,  :sql_file
-  alias_method_chain :count_by_sql, :sql_file
-end
-
+require File.dirname(__FILE__) + '/rails/init'
---
+++ rails/init.rb
@@ -0,0 +1,8 @@
+require 'bunster/find_by_sql_file'
+
+class << ActiveRecord::Base
+  include Bunster::FindBySqlFile
+
+  alias_method_chain :find_by_sql,  :sql_file
+  alias_method_chain :count_by_sql, :sql_file
+end
