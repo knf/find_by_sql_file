d290abf909659d8bb7a6740dcd751f3252a72dfc
Jordi Bunster <jordi@bunster.org>
2010-02-21 09:50:44 -0500

    No one does this in Ruby anymore...

--- lib/find_by_sql_file.rb
+++ lib/find_by_sql_file.rb
@@ -1,7 +1,67 @@
-require 'bunster/find_by_sql_file'
+require 'erb'
+
+module FindBySqlFile
+  # This makes it a tad simpler to create a clean ERB binding context. It
+  # works like so:
+  #
+  # ERBJacket.wrap "A Foo is: <%= @foo -%>", :foo => 'not a bar'
+  #
+  class ERBJacket
+    def initialize(hash) # :nodoc:
+      hash.each { |k, v| instance_variable_set('@' + k.to_s, v) }
+    end
+
+    def wrap(erb_template) # :nodoc:
+      ::ERB.new(erb_template, nil, '-').result binding
+    end
+
+    def self.wrap(erb_template = '', instance_variables = {}) # :nodoc:
+      new(instance_variables).wrap erb_template
+    end
+  end
+
+  # The path to the directory where queries will be found, organized
+  # inside directories named like their main tables (or 'application'
+  # for shared queries)
+  SQL_PATH = File.join RAILS_ROOT, 'app', 'queries'
+
+  def find_by_sql_with_sql_file(query_or_symbol, opts = {}) # :nodoc:
+    find_by_sql_without_sql_file sql_file(query_or_symbol, opts)
+  end
+
+  def count_by_sql_with_sql_file(query_or_symbol, opts = {}) # :nodoc:
+    count_by_sql_without_sql_file sql_file(query_or_symbol, opts)
+  end
+
+  def sql_file(query_or_symbol, opts = {}) # :nodoc:
+    if query_or_symbol.is_a? Symbol
+
+      file_name = File.join SQL_PATH,
+        (table_name rescue 'application'), (query_or_symbol.to_s + '.sql')
+
+      bound_variables = HashWithIndifferentAccess.new(opts).symbolize_keys!
+      injected_locals = bound_variables.delete(:inject!) || []
+
+      query = ERBJacket.wrap File.read(file_name), injected_locals
+      query = replace_named_bind_variables(query, bound_variables)
+
+      query.gsub(/-- .*/, '').strip.gsub(/\s+/, ' ')
+    else
+      raise 'Additional parameters only supported when using a query' \
+            ' file (pass a symbol, not a string)' unless opts.blank?
+
+      query_or_symbol
+    end
+  end
+
+  # The equivalent of ActiveRecord::Base#find_by_sql(*args).first
+  def find_one_by_sql(*args)
+    find_by_sql(*args).first
+  end
+end
 
 class << ActiveRecord::Base
-  include Bunster::FindBySqlFile
+  include FindBySqlFile
 
   alias_method_chain :find_by_sql,  :sql_file
   alias_method_chain :count_by_sql, :sql_file
